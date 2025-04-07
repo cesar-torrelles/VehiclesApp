bb3ea459612cabb0546681467bbe428615a0fc5e
cesar-torrelles <raseksk8@gmail.com>
2025-04-07 21:58:04 +0200

    puerto configurado

--- exercise-backend/src/main/java/com/vehicles/exercise/ApplicationConfig.java
+++ exercise-backend/src/main/java/com/vehicles/exercise/ApplicationConfig.java
@@ -3,6 +3,8 @@ package com.vehicles.exercise;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ApplicationConfig {
@@ -17,4 +19,20 @@ public class ApplicationConfig {
 	public DBInitializer dbInitializer() {
 		return new DBInitializer();
 	}
+   
+   
+   @Bean
+   public WebMvcConfigurer corsConfigurer() {
+     return new WebMvcConfigurer() {
+       @Override
+       public void addCorsMappings(CorsRegistry registry) {
+         registry.addMapping("/**")
+             .allowedOrigins("http://localhost:4200")
+             .allowedMethods("GET", "POST", "PUT", "DELETE")
+             .allowedHeaders("*");
+       }
+     };
+   }
+   
+   
 }
